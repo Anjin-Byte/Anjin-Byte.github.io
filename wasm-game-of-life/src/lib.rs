68ab7140f25ac7f8069f2264a242dbf6c3f1ee2c
Anjin-Byte <thalesarkanzen@gmail.com>
2024-08-16 15:51:11 -0500

    modified lib.rs

--- wasm-game-of-life/src/lib.rs
+++ wasm-game-of-life/src/lib.rs
@@ -2,6 +2,24 @@ mod utils;
 
 use wasm_bindgen::prelude::*;
 
+
+#[wasm_bindgen]
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Cell {
+    Dead = 0,
+    Alive = 1,
+}
+
+
+#[wasm_bindgen]
+pub struct Universe {
+    width: u32,
+    height: u32,
+    cells: Vec<Cell>,
+}
+
+
 #[wasm_bindgen]
 extern "C" {
     fn alert(s: &str);
