6ff8287e72b9a2e20678dd9095100f76ad332b1d
Anjin-Byte <thalesarkanzen@gmail.com>
2024-08-16 17:07:42 -0500

    updated paths again

--- wasm-game-of-life/www/index.js
+++ wasm-game-of-life/www/index.js
@@ -1,5 +1,5 @@
-import * as wasm from "wasm-game-of-life";
-import { Universe } from "wasm-game-of-life";
+import * as wasm from "./wasm-game-of-life";
+import { Universe } from "./wasm-game-of-life";
 
 const pre = document.getElementById("game-of-life-canvas");
 const universe = Universe.new();
