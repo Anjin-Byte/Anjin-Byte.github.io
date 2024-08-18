a65c8702661e7630f34371d3d5b50a4a620ab428
Anjin-Byte <thalesarkanzen@gmail.com>
2024-08-18 13:07:30 -0500

    Modifed wasm init js

--- wasm-game-of-life/www/index.js
+++ wasm-game-of-life/www/index.js
@@ -1,7 +1,17 @@
 import * as wasm from "../pkg/wasm_game_of_life.js";
 import { Universe } from "../pkg/wasm_game_of_life.js";
 
+import __wbg_init from "../pkg/wasm_game_of_life.js"
+
 const pre = document.getElementById("game-of-life-canvas");
+
+async function start() {
+    // Initialize the WebAssembly module asynchronously
+    const wasm = await __wbg_init("../pkg/wasm_game_of_life_bg.wasm");
+}
+
+start();
+
 const universe = Universe.new();
 
 const renderLoop = () => {
