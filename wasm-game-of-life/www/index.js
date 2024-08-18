3c824934ec66d17ae0fe2b087b6e11e083e3c1bf
Anjin-Byte <thalesarkanzen@gmail.com>
2024-08-18 17:18:14 -0500

    Made draw grid function inner

--- wasm-game-of-life/www/index.js
+++ wasm-game-of-life/www/index.js
@@ -6,55 +6,6 @@ const GRID_COLOR = "#CCCCCC";
 const DEAD_COLOR = "#FFFFFF";
 const ALIVE_COLOR = "#000000";
 
-const drawGrid = () => {
-    ctx.beginPath();
-    ctx.strokeStyle = GRID_COLOR;
-  
-    // Vertical lines.
-    for (let i = 0; i <= width; i++) {
-      ctx.moveTo(i * (CELL_SIZE + 1) + 1, 0);
-      ctx.lineTo(i * (CELL_SIZE + 1) + 1, (CELL_SIZE + 1) * height + 1);
-    }
-  
-    // Horizontal lines.
-    for (let j = 0; j <= height; j++) {
-      ctx.moveTo(0,                           j * (CELL_SIZE + 1) + 1);
-      ctx.lineTo((CELL_SIZE + 1) * width + 1, j * (CELL_SIZE + 1) + 1);
-    }
-  
-    ctx.stroke();
-};
-  
-const getIndex = (row, column) => {
-    return row * width + column;
-  };
-  
-  const drawCells = () => {
-    const cellsPtr = universe.cells();
-    const cells = new Uint8Array(memory.buffer, cellsPtr, width * height);
-  
-    ctx.beginPath();
-  
-    for (let row = 0; row < height; row++) {
-      for (let col = 0; col < width; col++) {
-        const idx = getIndex(row, col);
-  
-        ctx.fillStyle = cells[idx] === Cell.Dead
-          ? DEAD_COLOR
-          : ALIVE_COLOR;
-  
-        ctx.fillRect(
-          col * (CELL_SIZE + 1) + 1,
-          row * (CELL_SIZE + 1) + 1,
-          CELL_SIZE,
-          CELL_SIZE
-        );
-      }
-    }
-  
-    ctx.stroke();
-  };
-
 init().then(() => {
     // Construct the universe, and get its width and height.
     const universe = Universe.new();
@@ -69,13 +20,62 @@ init().then(() => {
 
     const ctx = canvas.getContext('2d');
 
+    const drawGrid = () => {
+        ctx.beginPath();
+        ctx.strokeStyle = GRID_COLOR;
+    
+        // Vertical lines.
+        for (let i = 0; i <= width; i++) {
+            ctx.moveTo(i * (CELL_SIZE + 1) + 1, 0);
+            ctx.lineTo(i * (CELL_SIZE + 1) + 1, (CELL_SIZE + 1) * height + 1);
+        }
+    
+        // Horizontal lines.
+        for (let j = 0; j <= height; j++) {
+            ctx.moveTo(0, j * (CELL_SIZE + 1) + 1);
+            ctx.lineTo((CELL_SIZE + 1) * width + 1, j * (CELL_SIZE + 1) + 1);
+        }
+    
+        ctx.stroke();
+    };
+    
+    const getIndex = (row, column) => {
+        return row * width + column;
+    };
+    
+    const drawCells = () => {
+        const cellsPtr = universe.cells();
+        const cells = new Uint8Array(memory.buffer, cellsPtr, width * height);
+    
+        ctx.beginPath();
+    
+        for (let row = 0; row < height; row++) {
+            for (let col = 0; col < width; col++) {
+                const idx = getIndex(row, col);
+    
+                ctx.fillStyle = cells[idx] === Cell.Dead
+                    ? DEAD_COLOR
+                    : ALIVE_COLOR;
+    
+                ctx.fillRect(
+                    col * (CELL_SIZE + 1) + 1,
+                    row * (CELL_SIZE + 1) + 1,
+                    CELL_SIZE,
+                    CELL_SIZE
+                );
+            }
+        }
+    
+        ctx.stroke();
+    };
+
     const renderLoop = () => {
-    universe.tick();
+        universe.tick();
 
-    drawGrid();
-    drawCells();
+        drawGrid();
+        drawCells();
 
-    requestAnimationFrame(renderLoop);
+        requestAnimationFrame(renderLoop);
     };
     //wasm.greet("Mae \u{1F48C} - I got website running! U are a goof");
 });
