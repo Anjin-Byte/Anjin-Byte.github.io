394b47006236c61815e8ea5667a49dcc7102eb47
Anjin-Byte <thalesarkanzen@gmail.com>
2024-08-18 14:20:19 -0500

    Moved index.html

--- wasm-game-of-life/www/index.js
+++ wasm-game-of-life/www/index.js
@@ -2,7 +2,6 @@ import * as wasm from "../pkg/wasm_game_of_life.js";
 import init, { Universe } from "../pkg/wasm_game_of_life.js";
 
 init().then(() => {
-    console.log("wasm init")
     const pre = document.getElementById("game-of-life-canvas");
 
     const universe = Universe.new();
@@ -21,7 +20,6 @@ init().then(() => {
 
 
 /*
-Get rid of favicon 404 error
 Get rid of redirect
 Change message to Mae
 */
