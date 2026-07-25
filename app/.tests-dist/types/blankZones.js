ab61dc0b7c6ed20c53f179a62e93d02f7596d654
Anjin-Byte <hale.taylor.dev@gmail.com>
2026-07-25 15:08:30 -0500

    add generic worker feature channel, untrack stale build artifacts

--- app/.tests-dist/types/blankZones.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MAX_BLANK_ZONES = exports.BLANK_ZONE_STORAGE_KEY = exports.BLANK_ZONE_STORAGE_VERSION = exports.asZoneId = void 0;
-const asZoneId = (s) => s;
-exports.asZoneId = asZoneId;
-exports.BLANK_ZONE_STORAGE_VERSION = 1;
-exports.BLANK_ZONE_STORAGE_KEY = `gol.gridBlankZones.v${exports.BLANK_ZONE_STORAGE_VERSION}`;
-exports.MAX_BLANK_ZONES = 128;
